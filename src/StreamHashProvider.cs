96a42f4536c76a513407d846eb2004cbe72bc941
simon-techkid <simon.techkid@outlook.com>
2024-11-03 16:39:59 -0800

    v1.3.0

---
+++ src/StreamHashProvider.cs
@@ -0,0 +1,20 @@
+﻿// HashingHandler by Simon Field
+
+using System.IO;
+
+namespace HashingHandler.Formats.Stream;
+
+public class StreamHashProvider : IHashingProvider<System.IO.Stream>
+{
+    public byte[] ConvertToBytes(System.IO.Stream data)
+    {
+        byte[] buffer = new byte[1024];
+        using MemoryStream memoryStream = new();
+        int read;
+        while ((read = data.Read(buffer, 0, buffer.Length)) > 0)
+        {
+            memoryStream.Write(buffer, 0, read);
+        }
+        return memoryStream.ToArray();
+    }
+}
---
+++ src/StreamHashVerifier.cs
@@ -0,0 +1,8 @@
+﻿// HashingHandler by Simon Field
+
+namespace HashingHandler.Formats.Stream;
+
+public class StreamHashVerifier : HashVerifierBase<System.IO.Stream>
+{
+    protected override IHashingProvider<System.IO.Stream> HashProvider => new StreamHashProvider();
+}
