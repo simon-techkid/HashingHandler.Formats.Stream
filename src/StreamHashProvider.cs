9fbea6bbfec59639978053c1227c6852af3ea06a
simon-techkid <simon.techkid@outlook.com>
2024-11-05 00:19:47 -0800

    v1.4.0

--- src/StreamHashProvider.cs
+++ src/StreamHashProvider.cs
@@ -1,20 +1,66 @@
 ﻿// HashingHandler by Simon Field
 
+using System;
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace HashingHandler.Formats.Stream;
 
-public class StreamHashProvider : IHashingProvider<System.IO.Stream>
+public class StreamHashProvider : IHashingProviderAsync<System.IO.Stream>
 {
+    private readonly int _bufferSize;
+
+    public StreamHashProvider(int bufferSize = 8192)
+    {
+        if (bufferSize <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(bufferSize), "Buffer size must be greater than zero.");
+        }
+
+        _bufferSize = bufferSize;
+    }
+
     public byte[] ConvertToBytes(System.IO.Stream data)
     {
-        byte[] buffer = new byte[1024];
+        if (data == null) throw new ArgumentNullException(nameof(data));
+
+        return ReadStream(data);
+    }
+
+    public async Task<byte[]> ConvertToBytesAsync(System.IO.Stream data, CancellationToken cancellationToken = default)
+    {
+        if (data == null) throw new ArgumentNullException(nameof(data));
+
+        return await ReadStreamAsync(data, cancellationToken);
+    }
+
+    private byte[] ReadStream(System.IO.Stream data)
+    {
         using MemoryStream memoryStream = new();
-        int read;
-        while ((read = data.Read(buffer, 0, buffer.Length)) > 0)
+        byte[] buffer = new byte[_bufferSize];
+        int bytesRead;
+
+        while ((bytesRead = data.Read(buffer, 0, buffer.Length)) > 0)
         {
-            memoryStream.Write(buffer, 0, read);
+            memoryStream.Write(buffer, 0, bytesRead);
         }
+
+        return memoryStream.ToArray();
+    }
+
+    private async Task<byte[]> ReadStreamAsync(System.IO.Stream data, CancellationToken cancellationToken)
+    {
+        using MemoryStream memoryStream = new();
+        byte[] buffer = new byte[_bufferSize];
+        int bytesRead;
+
+        while ((bytesRead = await data.ReadAsync(buffer, 0, buffer.Length, cancellationToken)) > 0)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            await memoryStream.WriteAsync(buffer, 0, bytesRead, cancellationToken);
+        }
+
         return memoryStream.ToArray();
     }
 }
--- src/StreamHashVerifier.cs
+++ src/StreamHashVerifier.cs
@@ -4,5 +4,22 @@ namespace HashingHandler.Formats.Stream;
 
 public class StreamHashVerifier : HashVerifierBase<System.IO.Stream>
 {
-    protected override IHashingProvider<System.IO.Stream> HashProvider => new StreamHashProvider();
+    /// <summary>
+    /// Construct a <see cref="StreamHashVerifier"/> object with the default buffer size.
+    /// </summary>
+    public StreamHashVerifier()
+    {
+        HashProvider = new StreamHashProvider();
+    }
+
+    /// <summary>
+    /// Construct a <see cref="StreamHashVerifier"/> object with the given buffer size, <paramref name="bufferSize"/>.
+    /// </summary>
+    /// <param name="bufferSize"></param>
+    public StreamHashVerifier(int bufferSize)
+    {
+        HashProvider = new StreamHashProvider(bufferSize);
+    }
+
+    protected override IHashingProvider<System.IO.Stream> HashProvider { get; }
 }
